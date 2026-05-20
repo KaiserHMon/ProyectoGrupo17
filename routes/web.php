dd5e954298e2a0d852a4d5abf8e4514cf70b76a3
KaiserHMon <secondhardoy@gmail.com>
2026-05-20 16:31:37 -0300

    feat: update catalog route to use ProductoController

--- routes/web.php
+++ routes/web.php
@@ -3,49 +3,40 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\ConsultaController;
+use App\Http\Controllers\ProductoController;
 
-// Ruta principal - muestra la página de inicio
 Route::get('/', function () {
     return view('principal');
 });
 
-// Muestra la página de términos y condiciones
 Route::get('/terminos-de-uso', function () {
     return view('terminos-de-uso');
 });
 
-// Muestra el formulario de contacto
 Route::get('/contacto', function () {
     return view('contacto');
 });
 
-// Procesa el envío del formulario de contacto
 Route::post('/contacto', [ContactoController::class, 'procesar']);
 
-// Muestra la página de comercialización
 Route::get('/comercializacion', function () {
     return view('comercializacion');
 });
 
-// Muestra el catálogo de productos
-Route::get('/catalogo', function () {
-    return view('catalogo');
-});
+# Agregar middleware para que haya que iniciar sesion para ver el catalogo
+Route::get('/catalogo', [ProductoController::class, 'index']);
 
-// Muestra información sobre la empresa
 Route::get('/quienes-somos', function() {
     return view('quienes-somos');
 });
 
-// Muestra la página de login y registro
 Route::get('/login-register', function() {
     return view('login-register');
 });
 
-// Muestra el formulario de consultas
+
 Route::get('/consultas', function() {
     return view('consultas');
 });
 
-// Procesa el envío del formulario de consultas
 Route::post('/consultas', [ConsultaController::class, 'procesar']);
