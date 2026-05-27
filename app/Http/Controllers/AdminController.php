58f3f86044149d4ac87628600ec1ef686d512ce3
Benja Harvey <benjaharvey@gmail.com>
2026-05-27 16:37:29 -0300

    creo vista de usuarios registrados en el panel de administrador, genero vista de prueba dashboard cliente

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Producto;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function dashboard(){
         $productos = Producto::all();
-        return view('backend.admin.dashboard', compact('productos'));
+        $usuarios = Usuario::with('rol')->get();
+        return view('backend.admin.dashboard', compact('productos', 'usuarios'));
     }
 }
---
+++ resources/views/backend/dashboard_cliente.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('contenido')
+
+<h1>Hola</h1>
+
+@endsection
