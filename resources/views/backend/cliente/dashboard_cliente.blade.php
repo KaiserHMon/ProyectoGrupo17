11d79a99a646f545fa1d6c196dc8a63081fb25a4
Benja Harvey <benjaharvey@gmail.com>
2026-06-03 16:44:09 -0300

    commit para pasarlo a pc de escritorio

--- resources/views/backend/cliente/dashboard_cliente.blade.php
+++ resources/views/backend/cliente/dashboard_cliente.blade.php
@@ -2,6 +2,17 @@
 
 @section('contenido')
 
-<h1>Vista cliente - Prueba</h1>
+<section class="py-5" style="background-color: #FCF9F4; min-height: 80vh;">
+    <div class="container">
+        <div class="mb-5 text-center">
+            <h1>Bienvenido - Nombre cliente</h1>
+            <p class="text-muted">Aca vas a encontrar tu información personal y tu historial de compras</p>
+        </div>
+
+        <div class="row g-4">
+            
+        </div>
+    </div>
+</section>
 
 @endsection
